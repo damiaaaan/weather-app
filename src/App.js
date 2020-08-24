017f7bd94b790e6411cc00e50198a8575cf68e60
Damian Bertoni <damianberto@gmail.com>
2020-08-24 09:07:30 -0300

    Refactor: convert method to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,46 +6,55 @@ import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import {Grid, Col, Row} from 'react-flexbox-grid';
 import LocationList from './components/WeatherLocation/LocationList';
+import ForecastExtended from './components/ForecastExtended';
 
 const cities = [
   'Buenos Aires,ar',
   'Cambridge,gb',
-  'London,gb',
+  'Finland,fi',
   'Asuncion,py',
   'Ciudad de Mexico,mx'
 ]
 
 
-function App() {
+class App extends Component {
 
-  const handleLocationSelection = city => {
+  constructor() {
+    super();
+    this.state = { city: 'Nueva ciudad.' };
+  }
+
+  handleLocationSelection = city => {
     console.log(city)
   };
-
-  return (
-    <Grid>
-      <Row>
-        <AppBar position='sticky'>
-          <Toolbar>
-            <Typography variant='inherit' color='inherit'>
-              Weather App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Row>
-      <Row>
-        <Col xs={12} md={6}>
-          <LocationList cities={cities} 
-          onSelectedLocation={handleLocationSelection} ></LocationList>
-        </Col>
-        <Col xs={12} md={6}>
-          <Paper elevation={4}>
-            <div className="details"></div>
-          </Paper>
-        </Col>
-      </Row>
-    </Grid>
-  );
+  render() {
+    const { city } = this.state;
+    return (
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='inherit' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList cities={cities} 
+            onSelectedLocation={this.handleLocationSelection} ></LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details">
+                <ForecastExtended city={ city }></ForecastExtended>
+              </div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
-
 export default App;
