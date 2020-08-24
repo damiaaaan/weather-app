ecdc07ececcf59c2b73e9a2f3ffdd3abb00b0ef6
Damian Bertoni <damianberto@gmail.com>
2020-08-24 09:08:15 -0300

    New component, forecatExtended to show the extended weather

---
+++ src/components/ForecastExtended.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ForecastExtended extends Component {
+    render() {
+        const { city } = this.props;
+        return (<div>Pronostico extendido para {city}</div>);
+    }
+}
+
+ForecastExtended.propTypes = {
+    city: PropTypes.string.isRequired,
+}
+
+export default ForecastExtended;
+
+
